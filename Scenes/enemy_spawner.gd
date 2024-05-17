0363e508e2c558ac6d40931a75307c9bef300224
Emre Chakar <emrechakar@outlook.com>
2024-05-18 02:38:42 +0300

    update

---
+++ Scenes/enemy_spawner.gd
@@ -0,0 +1,13 @@
+extends Node2D
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	$Timer.timeout.connect(on_timer_timeout)
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+func on_timer_timeout():
+	print("spawn")
