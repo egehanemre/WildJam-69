24cae200d1d3bd680cd72eb23c2ed1574384940c
Egehan EMRE <16008122049@ogr.bozok.edu.tr>
2024-05-19 15:37:36 +0300

    fix

--- Scenes/Shields.gd
+++ Scenes/Shields.gd
@@ -1,7 +1,10 @@
 extends Node2D
 
+signal currentHealth(value)
+
 var currentArea : int
 var shieldPower : int
+var health : int = 100
 
 func _ready():
 	var tween = create_tween()
@@ -11,6 +14,7 @@ func _ready():
 
 func _physics_process(delta):
 	if currentArea == 1:
+		$ShieldSprite/ShieldBody/CollisionShape2D.disabled = false
 		if shieldPower == 3:
 			var tween = create_tween()
 			tween.set_ease(Tween.EASE_IN_OUT)
@@ -31,6 +35,7 @@ func _physics_process(delta):
 			tween.set_ease(Tween.EASE_IN_OUT)
 			tween.tween_property($ShieldSprite, "scale", Vector2(0, 0), 0.1)
 	else:
+		$ShieldSprite/ShieldBody/CollisionShape2D.disabled = true
 		var tween = create_tween()
 		tween.set_ease(Tween.EASE_IN_OUT)
 		tween.tween_property($ShieldSprite, "scale", Vector2(0, 0), 0.1)
@@ -40,3 +45,22 @@ func _on_player_current_area_signal(value):
 
 func _on_energy_system_current_shield(value):
 	shieldPower = value
+
+func _on_shield_body_area_entered(area):
+	if area.is_in_group("golem") and currentArea == 1:
+		area.queue_free()
+		do_damage(30)
+		
+func _on_area_2d_area_entered(area):
+	if area.is_in_group("golem"):
+		area.queue_free()
+		do_damage(30)
+
+func do_damage(dmg : int):
+	
+	if shieldPower != 0:
+		health -= dmg / shieldPower
+		emit_signal("currentHealth",health)
+	else:
+		health -= 40	
+		emit_signal("currentHealth",health)
--- Scripts/UI.gd
+++ Scripts/UI.gd
@@ -2,9 +2,12 @@ extends Control
 
 var shieldPower : int
 var laserPower : int
+var healthPower : int = 100
 
 @onready var label = $Label
 @onready var label_2 = $Label2
+@onready var label_3 = $Label3
+
 
 func _ready():
 	laserPower = 3
@@ -13,9 +16,16 @@ func _ready():
 func _process(delta):
 	label.text = "ShieldPower: " + str(shieldPower)
 	label_2.text = "Laser Power:" + str(laserPower)
+	label_3.text = "Health: " + str(healthPower)
 
 func _on_energy_system_current_laser(value):
 	laserPower = value
 
 func _on_energy_system_current_shield(value):
 	shieldPower = value
+
+func _on_shields_current_health(value):
+	healthPower = value
+	if healthPower < 1:
+		healthPower = 0
+		print("gameover")
