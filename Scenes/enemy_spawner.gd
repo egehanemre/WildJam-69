98851a8e532e172d36b98710194900e89c36e1b2
Emre Chakar <emrechakar@outlook.com>
2024-05-19 14:14:24 +0300

    Some fixing to enemy spawner

--- Scenes/enemy_spawner.gd
+++ Scenes/enemy_spawner.gd
@@ -1,8 +1,7 @@
 extends Node2D
 
 @export var projectile_scene:PackedScene
-@export var small_radius: int = 100
-@export var big_radius: int = 100
+@export var radius: int = 100
 @export var big_enemy: bool
 
 # Called when the node enters the scene tree for the first time.
@@ -19,7 +18,7 @@ func on_small_timer_timeout():
 	if big_enemy == true:
 		return
 	var random_direction = Vector2.LEFT.rotated(randf_range(0, PI))
-	var spawn_position = global_position + (random_direction * small_radius)
+	var spawn_position = global_position + (random_direction * radius)
 	
 	var projectile = projectile_scene.instantiate() as Node2D
 	get_parent().add_child(projectile)
@@ -30,7 +29,7 @@ func on_big_timer_timeout():
 		return
 		
 	var random_direction = Vector2.LEFT.rotated(randf_range(0, PI))
-	var spawn_position = global_position + (random_direction * big_radius)
+	var spawn_position = global_position + (random_direction * radius)
 	
 	var projectile = projectile_scene.instantiate() as Node2D
 	get_parent().add_child(projectile)
