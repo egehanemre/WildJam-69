39eec0aa4fa174d239b4ed570bc2e6bd021ec7bc
Emre Chakar <emrechakar@outlook.com>
2024-05-19 14:10:58 +0300

    Enemy spawener update

--- Scenes/enemy_spawner.gd
+++ Scenes/enemy_spawner.gd
@@ -1,13 +1,37 @@
 extends Node2D
 
+@export var projectile_scene:PackedScene
+@export var small_radius: int = 100
+@export var big_radius: int = 100
+@export var big_enemy: bool
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	$Timer.timeout.connect(on_timer_timeout)
+	$SmallEnemy.timeout.connect(on_small_timer_timeout)
+	$BigEnemy.timeout.connect(on_big_timer_timeout)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
 
-func on_timer_timeout():
-	pass
+func on_small_timer_timeout():
+	if big_enemy == true:
+		return
+	var random_direction = Vector2.LEFT.rotated(randf_range(0, PI))
+	var spawn_position = global_position + (random_direction * small_radius)
+	
+	var projectile = projectile_scene.instantiate() as Node2D
+	get_parent().add_child(projectile)
+	projectile.global_position = spawn_position
+	
+func on_big_timer_timeout():
+	if big_enemy == false:
+		return
+		
+	var random_direction = Vector2.LEFT.rotated(randf_range(0, PI))
+	var spawn_position = global_position + (random_direction * big_radius)
+	
+	var projectile = projectile_scene.instantiate() as Node2D
+	get_parent().add_child(projectile)
+	projectile.global_position = spawn_position
